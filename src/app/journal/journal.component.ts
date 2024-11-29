9c6ccf96abb8dc2cfdfd8790487879e9f1254ca9
MitjaCH <mitja.kurath@mitja.io>
2024-11-29 09:02:21 +0100

    Implement Markdown-to-HTML transformation in journal component with basic markdown file

--- src/app/journal/journal.component.ts
+++ src/app/journal/journal.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MarkdownService } from '../services/markdown.service';
 
 @Component({
   selector: 'app-journal',
-  imports: [],
   templateUrl: './journal.component.html',
-  styleUrl: './journal.component.css'
+  styleUrls: ['./journal.component.css']
 })
 export class JournalComponent {
+  private mdService = inject(MarkdownService);
 
+  rawMarkdown: string = `# Welcome to Reflective.md
+  This is **bold** text and this is *italic* text.
+
+  - Item 1
+  - Item 2
+
+  [Visit Google](https://www.google.com)
+
+  \`\`\`
+  let x = 10;
+  \`\`\``;
+
+  htmlContent: string = '';
+
+  async ngOnInit() {
+    this.htmlContent = await this.mdService.transformMarkdown(this.rawMarkdown);
+  }
 }
